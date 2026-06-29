f20c7bf0d6375b1b832f1fdc1cbf7c8551cef542
VireArcanist <virelyn@outlook.de>
2026-06-29 08:18:27 +0200

    Create class signs and constructor

---
+++ model/signs.java
@@ -0,0 +1,12 @@
+package model;
+
+public class signs {
+    public String sign;
+    public int numbers;
+
+    public signs(String sign, int numbers) {
+        this.sign = sign;
+        this.numbers = numbers;
+    }
+    
+}
